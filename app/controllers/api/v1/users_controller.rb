34e89d32d1d3217cab0c2618bf538777304342e2
Oleg Lagunkov <o.lagunkov@gmail.com>
2022-10-30 21:45:30 +0400

    Add CRUD for api/v1/users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        users = Application.db[:users].all
+
+        render json: users
+      end
+
+      def update
+        user = Application.db[:users].where(id: params[:id]).first
+        params = user.merge(json_params)
+        Application.db[:users].where(id: params[:id]).update(params)
+
+        render head: 204
+      end
+
+      def create
+        Application.db[:users].insert(json_params)
+
+        render head: 204
+      end
+
+      def show
+        user = Application.db[:users].where(id: params[:id]).first
+
+        render json: user
+      end
+
+      def delete
+        Application.db[:users].where(id: params[:id]).delete
+
+        render head: 204
+      end
+    end
+  end
+end
