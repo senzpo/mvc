5661159a69abc7ea77c481a209c71267991c5b76
Oleg Lagunkov <o.lagunkov@gmail.com>
2022-12-01 01:44:25 +0400

    Not working session spec

---
+++ spec/controllers/web/sessions_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Web::SessionsController' do
+  let(:app) { Application.new }
+  let(:password) { 'awesome_pass' }
+  let(:password_hash) { '$2a$12$IsIjIHptwtYatJU7UXHOzu5praukAbElPvkc3i5i/3wJGBCP/5xKq' }
+  let(:salt) { '1960874c' }
+
+  let(:user_db_attributes) { {
+    email: 'example@example.com',
+    password_hash: password_hash,
+    salt: salt
+  } }
+
+  let(:user_attributes) { {
+    email: 'example@example.com',
+    password: password
+  } }
+
+  xit 'create' do
+    ApplicationRepository::DB[:users].insert(user_db_attributes)
+    env = Rack::MockRequest.env_for(
+      '/sessions',
+      'REQUEST_METHOD' => 'POST',
+      params: user_attributes
+    )
+    response = app.call(env)
+  end
+end
