3030a00a26baebce577b2efd3b4cb9d10c1a5a65
Evgenii Sendziuk <evgeniisendziuk@taxdome.com>
2023-01-19 19:00:39 +0300

    Done base serializer

--- lib/serializers/application_serializer.rb
+++ lib/serializers/application_serializer.rb
@@ -2,65 +2,57 @@
 
 # All app serializers must be subclasses of ApplicationSerializer
 class ApplicationSerializer
-  attr_reader :entity
+  class EntitySerializer
+    attr_reader :entity, :attributes
+
+    def initialize(entity, attributes)
+      @entity = entity
+      @attributes = attributes
+    end
+
+    def to_h
+      result_attributes = {}
+      attributes.each do |attr|
+        result_attributes[attr] = get_attribute(attr)
+      end
+      { type: type, id: id, attributes: result_attributes }
+    end
+
+    private
+
+    def get_attribute(attr)
+      if respond_to?(attr)
+        send(attr)
+      elsif entity.respond_to?(attr)
+        entity.send(attr)
+      else
+        raise NoMethodError, "Undefined serialize key #{attr}"
+      end
+    end
+
+    def type
+      entity.class.to_s.downcase
+    end
+
+    def id
+      entity.id.to_s
+    end
+  end
+
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def to_h
+    result = data.instance_of?(Array) ? data.map { |e| EntitySerializer.new(e, attributes).to_h } : EntitySerializer.new(data, attributes).to_h
+    { data: result }
+  end
 
   def self.attributes(*args)
     define_method :attributes do
       args
     end
-  end
-
-  def self.type(entity_type)
-    define_method :type do
-      entity_type
-    end
-  end
-
-  def initialize(object)
-    object.instance_of?(Array) ? @entities = object : @entity = object
-  end
-
-  def to_h
-    data = @entities ? data_for_array : data_for_single_entity
-    { data: data }
-  end
-
-  def data_for_array
-    result_data = []
-    @entities.each do |entity|
-      @entity = entity
-      result_attributes = {}
-      attributes.each do |attr|
-        result_attributes[attr] = get_attribute(attr)
-      end
-      result_data.push({ type: type, id: id, attributes: result_attributes })
-    end
-    result_data
-  end
-
-  def data_for_single_entity
-    result_attributes = {}
-    attributes.each do |attr|
-      result_attributes[attr] = get_attribute(attr)
-    end
-    { type: type, id: id, attributes: result_attributes }
-  end
-
-  def get_attribute(attr)
-    if respond_to?(attr)
-      send(attr)
-    elsif entity.respond_to?(attr)
-      entity.send(attr)
-    else
-      raise NoMethodError, "Undefined serialize key #{attr}"
-    end
-  end
-
-  def type
-    entity.class.to_s.downcase
-  end
-
-  def id
-    entity.id.to_s
   end
 end
